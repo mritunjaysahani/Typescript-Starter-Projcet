4a85956a807b9840dbb428a32bc19970e3706b3b
yuvraj@Mritunjay <mrityunjayshn@gmail.com>
2026-07-03 18:45:54 +0000

    fix npm run issue

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express from 'express';
+import { serverconfig as serverConfig} from './config/config';
+import v1Router from './routes/v1/index.router';
+import v2Router from './routes/v2/index.router';
+import { appErrorHandler, genericErrorHandler } from './middleware/error.middleware';
+import logger from './config/logger.config';
+import { attachCorrelationIdMiddleware } from './middleware/corelation.middleware';
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello from TypeScript");
-});
 
-app.listen(3000, () => {
-    console.log("Server started");
+app.use(express.json());
+
+/**
+ * Registering all the routers and their corresponding routes with out app server object.
+ */
+
+app.use(attachCorrelationIdMiddleware);
+app.use('/api/v1', v1Router);
+app.use('/api/v2', v2Router); 
+
+
+/**
+ * Add the error handler middleware
+ */
+
+app.use(appErrorHandler);
+app.use(genericErrorHandler);
+
+
+app.listen(serverConfig.PORT, () => {
+    logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
+    logger.info(`Press Ctrl+C to stop the server.`);
 });
